7aad95ad864233d6116db0326edab6d0ca13cc8f
madan_naik <naikmadan49@gmail.com>
2022-04-05 21:28:20 +0530

    added draw

--- script.js
+++ script.js
@@ -5,12 +5,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const playerTurnTxt = document.getElementById('player__turn');
     const playerScoreTxt = document.getElementById('player_score');
     const endBtn = document.getElementById('end_game');
-    /*
-        Indexes within the board
-        [0] [1] [2]
-        [3] [4] [5]
-        [6] [7] [8]
-     */
+    const modal = document.getElementById("myModal");
+    const span = document.getElementById("close_t");
+
 
     // variables
     const winningCondition = [
@@ -27,15 +24,35 @@ window.addEventListener('DOMContentLoaded', () => {
     let playerTurn = "X";
     let playerXScore = 0;
     let playerOScore = 0;
+
+
     // Listeners
+
     tile.forEach((sTile, index) => {
         sTile.addEventListener("click", () => markBoard(sTile, index))
     })
 
+    function closeModal() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+        playerOScore = 0;
+        playerXScore = 0;
+        setScore()
+        clearBoard()
+    }
+    endBtn.addEventListener('click', endGame);
+
     document.getElementById('reset_game').addEventListener('click', clearBoard);
-    document.getElementById('end_game').addEventListener('click', endGame);
 
-
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
     // utility functions
     function clearBoard() {
         tile.forEach(sTile => {
@@ -87,6 +104,7 @@ window.addEventListener('DOMContentLoaded', () => {
     function endGame() {
         modal.style.display = "block";
         document.getElementById("game_details").innerHTML = playerXScore > playerOScore ? "Player X won" : "Player O won";
+
     }
     function checkDraw() {
         for (let i = 0; i < 9; i++) {
@@ -95,34 +113,6 @@ window.addEventListener('DOMContentLoaded', () => {
                 alert('Draw');
                 clearBoard();
             }
-        }
-    }
-
-    // model script code
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementById("close_t");
-    console.log(span.innerHTML);
-    // When the user clicks the button, open the modal 
-    btn.onclick = function () {
-        modal.style.display = "block";
-
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        alert('clicked')
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
         }
     }
 })
