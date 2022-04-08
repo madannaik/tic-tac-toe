b5f8f06f5f4d901640f24c8e71f121e844501441
madan_naik <naikmadan49@gmail.com>
2022-04-08 08:16:08 +0530

    added strike through feature

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let playerTurn = "X";
     let playerXScore = 0;
     let playerOScore = 0;
-
+    let isGameActive = true;
 
 
     // Listeners
@@ -60,9 +60,11 @@ window.addEventListener('DOMContentLoaded', () => {
         playerTurnTxt.innerHTML = `Player ${playerTurn} turn`;
         tile.forEach((sTile) => sTile.classList.remove('letter__X', 'letter__O'));
         board = ['', '', '', '', '', '', '', '', ''];
+        clearLine();
+        isGameActive = true;
     }
     function markBoard(sTile, index) {
-        if (sTile.innerHTML === '') {
+        if (isGameActive && sTile.innerHTML === '') {
             // update board
             board[index] = playerTurn;
             sTile.innerHTML = playerTurn;
@@ -85,11 +87,13 @@ window.addEventListener('DOMContentLoaded', () => {
                 continue;
             }
             if (f === s && s === t) {
+                drawLine(winningCondition[i][0], winningCondition[i][2], f);
                 f === 'X' ? ++playerXScore : ++playerOScore;
-                console.log(playerXScore, playerOScore);
                 setScore()
-                alert(`Player ${f} scored a point`);
-                clearBoard()
+                let amess = `Player ${f} scored a point`;
+                showSnackbar(amess);
+                isGameActive = false;
+                // clearBoard()
                 continue;
             }
             checkDraw();
@@ -124,6 +128,60 @@ window.addEventListener('DOMContentLoaded', () => {
         playerXScore = 0;
         setScore()
         clearBoard()
+    }
+    function drawLine(f, t, win) {
+        const parentelem = document.querySelectorAll('.board')[0].getBoundingClientRect();
+        const elem = document.querySelectorAll('.board__tile');
+        const strike = document.getElementById('board_strike');
+        const rect1 = elem[f].getBoundingClientRect();
+        const rect2 = elem[t].getBoundingClientRect();
+
+        const { x1, x2, y1, y2 } = getPoints(parentelem, rect1, rect2);
+
+        const angle = getAngle(x1, x2, y1, y2)
+
+        strike.style.top = 50 + rect1.top - parentelem.top + "px";
+        strike.style.left = rect1.left - parentelem.left + 48 + "px";
+        strike.style.transform = `rotate(${-angle + 'deg'})`;
+        (win === 'X')
+            ? strike.style.backgroundColor = "#045de9"
+            : strike.style.backgroundColor = "#e9902a";
+        strike.style.height = getDistance(x2, x1, y2, y1) + "px";
+    }
+    function clearLine() {
+        const strike = document.getElementById('board_strike');
+        strike.style.height = 0;
+    }
+    function getPoints(parent, rect1, rect2) {
+        let x1 = rect1.x - parent.x;
+        let x2 = rect2.x - parent.x;
+        let y1 = rect1.y - parent.y;
+        let y2 = rect2.y - parent.y;
+        return { x1, x2, y1, y2 };
+    }
+    // calculate the angle between points
+    function getAngle(x1, x2, y1, y2) {
+
+        // angle between two points
+        // θ = tan−1 (y2−y1)/(x2−x1)
+        // in degree
+        // deg = θ * 180/PI
+
+        let angle = Math.atan(((y2 - y1) / (x2 - x1)));
+        angle = angle * (180 / Math.PI);
+        if (Math.round(angle) === -45) {
+            return angle;
+        }
+        else { return 90 - angle }
+    }
+    function getDistance(x2, x1, y2, y1) {
+        return Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
+    }
+    function showSnackbar(text) {
+        var x = document.getElementById("snackbar");
+        x.className = "show";
+        x.innerHTML = text;
+        setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
     }
 })
 
